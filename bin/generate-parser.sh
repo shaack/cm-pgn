5a247ca8e935d217762f52fa5de545d627302351
shaack <shaack@gmail.com>
2018-03-07 21:29:09 +0100

    renamed parser folder

--- bin/generate-parser.sh
+++ bin/generate-parser.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-node_modules/pegjs/bin/pegjs --format globals --export-var pgnParser -o src/cm-pgn/generated/pgnParser.js src/grammar/pgn.pegjs
+node_modules/pegjs/bin/pegjs --format globals --export-var pgnParser -o src/cm-pgn/parser/pgnParser.js src/grammar/pgn.pegjs
