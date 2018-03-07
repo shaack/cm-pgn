fbd5ae10a4af17c080587eff5ed53e747ca1f079
shaack <shaack@gmail.com>
2018-03-07 21:23:24 +0100

    renamed generated parser and added to git

--- bin/generate-parser.sh
+++ bin/generate-parser.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-node_modules/pegjs/bin/pegjs --format globals --export-var parser -o src/cm-pgn/generated/parser.js src/grammar/pgn.pegjs
+node_modules/pegjs/bin/pegjs --format globals --export-var pgnParser -o src/cm-pgn/generated/pgnParser.js src/grammar/pgn.pegjs
