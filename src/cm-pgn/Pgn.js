cb7e249e8b3ad711d940e113d976f1977ebcfa54
Stefan Haack <shaack@gmail.com>
2021-01-14 21:35:51 +0100

    replaced endsWith() for compatibility

--- src/cm-pgn/Pgn.js
+++ src/cm-pgn/Pgn.js
@@ -5,7 +5,7 @@ export class Pgn {
 
     constructor(pgnString = "") {
         // only the header?
-        const lastHeaderElement =  pgnString.trim().endsWith("]") ? pgnString.length : pgnString.lastIndexOf("]\n\n") + 1
+        const lastHeaderElement =  pgnString.trim().substr(-1) === "]" ? pgnString.length : pgnString.lastIndexOf("]\n\n") + 1
         const headerString = pgnString.substr(0, lastHeaderElement)
         const historyString = pgnString.substr(lastHeaderElement)
         this.header = new Header(headerString)
