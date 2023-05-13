bd718a37c41c215f99819b39121dfe3f9171650b
shaack <shaack@gmail.com>
2023-05-13 21:52:02 +0200

    using nodeModulesUrl for the import

--- src/History.js
+++ src/History.js
@@ -4,7 +4,7 @@
  * License: MIT, see file 'LICENSE'
  */
 import {pgnParser} from "./parser/pgnParser.js"
-const {Chess} = await require("chess.mjs/src/Chess.js")
+const {Chess} = await import(nodeModulesUrl + "chess.mjs/src/Chess.js")
 
 function IllegalMoveException(fen, notation) {
     this.fen = fen
